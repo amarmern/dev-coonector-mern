1cf89dc1396f78cda5a93ec246268783b2cb5532
Amrendra Kumar <amarmca51@gmail.com>
2020-05-23 21:33:50 +0530

    added the functionality for Authentication

---
+++ routes/api/auth.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+//@route GET api/auth
+//desc Test route
+//@access Public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+//@route POST api/auth
+//@desc Authenticate user & get token
+//@access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'password is reuired').exists(),
+  ],
+  async (req, res) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      //see if user exits
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //ecrypt the password and then compare to match exists or not
+
+      const isMatch = await bycrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //res.send('Users Registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const connectDb = require('./config/db')
+
 const app = express();
 
+//Connect Data base
+connectDb();
+
+//Init middle ware
+
+app.use(express.json({extended : false}))
+
 app.get('/',(req,res)=> res.send('API Running'))
+
+//deine the Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 

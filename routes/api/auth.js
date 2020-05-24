f941a3be5b8b635a42070354f10bfd3b34d88681
Amrendra Kumar <amarmca51@gmail.com>
2020-05-24 21:39:59 +0530

    Added the Profile and uses features

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,7 +16,7 @@ router.get('/', auth, async (req, res) => {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
   } catch (err) {
-    console.log(err.message);
+    console.error(err.message);
     res.status(500).send('Server error');
   }
 });
@@ -73,7 +73,7 @@ router.post(
       );
       //res.send('Users Registered');
     } catch (err) {
-      console.log(err.message);
+      console.error(err.message);
       res.status(500).send('Server error');
     }
   }

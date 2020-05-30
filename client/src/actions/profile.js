48c6af1f3b92c571d430285e469e5d01ccc91474
Amrendra Kumar <amarmca51@gmail.com>
2020-05-30 22:47:27 +0530

    api corrected

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -175,7 +175,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
     try {
-      const res = await axios.delete(`/api/profile/profile`);
+      const res = await axios.delete(`/api/profile`);
 
       dispatch({
         type: CLEAR_PROFILE,
